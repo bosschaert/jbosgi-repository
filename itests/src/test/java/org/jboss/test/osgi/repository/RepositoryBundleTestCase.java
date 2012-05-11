94ae1642648c82d63e2bdcf62dcc275416090c06
Thomas Diesler <thomas.diesler@jboss.com>
2012-05-11 08:53:58 +0200

    Remove lgpl license headers

--- itests/src/test/java/org/jboss/test/osgi/repository/RepositoryBundleTestCase.java
+++ itests/src/test/java/org/jboss/test/osgi/repository/RepositoryBundleTestCase.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,22 +17,7 @@
  * limitations under the License.
  * #L%
  */
-/*
- * JBoss, Home of Professional Open Source
- * Copyright 2009, Red Hat Middleware LLC, and individual contributors
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jboss.test.osgi.repository;
 
 import static org.junit.Assert.assertEquals;
@@ -87,7 +72,7 @@ public class RepositoryBundleTestCase {
                 builder.addBundleSymbolicName(archive.getName());
                 builder.addBundleManifestVersion(2);
                 builder.addImportPackages(BundleActivator.class, Repository.class, Resource.class);
-                builder.addImportPackages(Repository.class, XCapability.class);
+                builder.addImportPackages(XCapability.class);
                 return builder.openStream();
             }
         });
