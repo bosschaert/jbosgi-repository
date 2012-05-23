558c3f6888aa229ef5b9df4862fa855076c5f22e
David Bosschaert <david.bosschaert@gmail.com>
2012-05-23 11:33:18 +0100

    Basic population works.

--- tck/src/main/java/org/jboss/test/osgi/repository/tck/Activator.java
+++ tck/src/main/java/org/jboss/test/osgi/repository/tck/Activator.java
@@ -19,19 +19,20 @@
  */
 package org.jboss.test.osgi.repository.tck;
 
-import java.net.URL;
+import java.io.ByteArrayInputStream;
+import java.util.Hashtable;
 
 import org.jboss.osgi.repository.RepositoryReader;
 import org.jboss.osgi.repository.RepositoryStorage;
 import org.jboss.osgi.repository.RepositoryXMLReader;
 import org.jboss.osgi.repository.XPersistentRepository;
 import org.jboss.osgi.resolver.XResource;
-import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.BundleEvent;
+import org.osgi.framework.Filter;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.repository.Repository;
-import org.osgi.util.tracker.BundleTracker;
 import org.osgi.util.tracker.ServiceTracker;
 
 /**
@@ -40,44 +41,38 @@ import org.osgi.util.tracker.ServiceTracker;
  * @author David Bosschaert
  */
 public class Activator implements BundleActivator {
-    private BundleTracker bundleTracker;
+    private static final String TEST_CLASS_NAME = "org.osgi.test.cases.repository.junit.RepositoryTest";
+
+    private ServiceTracker serviceTracker;
+    private ServiceRegistration servicePrimedRegistration;
 
     @Override
     public void start(final BundleContext context) throws Exception {
         System.err.println("********* in the integration bundle");
 
-        bundleTracker = new BundleTracker(context, Bundle.ACTIVE, null) {
+        Filter filter = context.createFilter(
+                "(&(objectClass=java.lang.String)(repository-xml=" + TEST_CLASS_NAME + "))");
+        serviceTracker = new ServiceTracker(context, filter, null) {
             @Override
-            public Object addingBundle(Bundle bundle, BundleEvent event) {
-                if (bundle.getSymbolicName().equals("org.osgi.test.cases.repository")) {
-                    primeRepository(context, bundle);
+            public Object addingService(ServiceReference reference) {
+                Object svc = super.addingService(reference);
+                if (svc instanceof String) {
+                    primeRepository(context, (String) svc);
                 }
-                return super.addingBundle(bundle, event);
+                return svc;
             }
         };
-        bundleTracker.open();
-        /*
-        bundleListener = new BundleListener() {
-            @Override
-            public void bundleChanged(BundleEvent event) {
-                Bundle b = event.getBundle();
-                if ("org.osgi.test.cases.repository".equals(event.getBundle().getSymbolicName())) {
-                    if (event.getType() == BundleEvent.STARTED) {
-                        primeRepository(context, b);
-                    }
-                }
-            }
-        };
-        context.addBundleListener(bundleListener);
-
-        // The bundle could have been activated before...
-        Bundle b = findBundle(context, "org.osgi.test.cases.repository");
-        if (b.getState() == Bundle.ACTIVE)
-         */
+        serviceTracker.open();
     }
 
-    public void primeRepository(BundleContext context, Bundle bundle) {
-        System.err.println("*********** priming from: " + bundle);
+    public void primeRepository(BundleContext context, String xml) {
+        synchronized (this) {
+            if (servicePrimedRegistration != null)
+                // not priming again, already primed
+                return;
+        }
+
+        System.err.println("*********** priming from: " + xml);
         ServiceTracker st = new ServiceTracker(context, Repository.class.getName(), null);
         st.open();
 
@@ -89,13 +84,19 @@ public class Activator implements BundleActivator {
             RepositoryStorage rs = xpr.getRepositoryStorage();
             System.err.println("*********** obtained Repository Storage: " + rs);
 
-            URL contentURL = bundle.getResource("xml/content1.xml");
-            RepositoryReader reader = RepositoryXMLReader.create(contentURL.openStream());
+            RepositoryReader reader = RepositoryXMLReader.create(new ByteArrayInputStream(xml.getBytes()));
             XResource resource = reader.nextResource();
             while (resource != null) {
                 rs.addResource(resource);
                 resource = reader.nextResource();
             }
+
+            Hashtable<String, Object> props = new Hashtable<String, Object>();
+            props.put("repository-populated", TEST_CLASS_NAME);
+            synchronized (this) {
+                servicePrimedRegistration = context.registerService(Object.class.getName(), new Object(), props);
+            }
+
             System.err.println("*********** finished priming repository storage: " + rs);
         } catch (Exception ex) {
             ex.printStackTrace(System.err);
@@ -105,21 +106,11 @@ public class Activator implements BundleActivator {
         }
     }
 
-
-
-    private Bundle findBundle(BundleContext ctx, String name) {
-        for (Bundle b : ctx.getBundles()) {
-            if (name.equals(b.getSymbolicName())) {
-                return b;
-            }
-        }
-        return null;
-    }
-
-
-
     @Override
-    public void stop(BundleContext context) throws Exception {
-        bundleTracker.close();
+    public synchronized void stop(BundleContext context) throws Exception {
+        serviceTracker.close();
+
+        if (servicePrimedRegistration != null)
+            servicePrimedRegistration.unregister();
     }
 }
